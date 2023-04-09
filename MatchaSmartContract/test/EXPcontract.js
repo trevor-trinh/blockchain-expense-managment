d292675e51d699b82db45a80898e8b365e281013
Derrick Cui <derrick_cui@berkeley.edu>
2023-04-08 21:16:55 -0700

    done contract!

--- MatchaSmartContract/test/EXPcontract.js
+++ MatchaSmartContract/test/EXPcontract.js
@@ -3,80 +3,73 @@ const {
 } = require("@nomicfoundation/hardhat-network-helpers");
 const { expect } = require("chai");
 
-describe("EXPcontract", function () {
+describe("Expense", function () {
 
   async function deployContract() {
     const [owner, addr1] = await ethers.getSigners();
 
-    const Expense = await ethers.getContractFactory("EXPcontract");
-    const hardhatExpense = await Expense.deploy();
+    //deploye MockUSDC
+    const MockUSDC = await ethers.getContractFactory("MockUSDC");
+    const mockUSDC = await MockUSDC.deploy();
 
-    return { hardhatExpense, owner, addr1 };
+    //deploye Expense
+    const Expense = await ethers.getContractFactory("Expense");
+    const hardhatExpense = await Expense.deploy(mockUSDC.address);//CHANGE THIS WHEN YOU DEPLOY TO MAINNET, NEED TO ADD ADDRESS OF USDC
+
+    // Mint some MockUSDC for testing purposes
+    await mockUSDC.mint(addr1.address, ethers.utils.parseUnits("100000", 6));
+    await hardhatExpense.mint(owner.address, ethers.utils.parseUnits("1000", 18));
+
+    return { hardhatExpense, owner, addr1, mockUSDC };
   }
 
   describe("testing", function () {
     //check ownership
-    it("Should set the right owner", async function () {
+    it("owner is correct", async function () {
       const { hardhatExpense, owner, addr1 } = await loadFixture(deployContract);
-
       expect(await hardhatExpense.owner()).to.equal(owner.address);
     });
 
     //test that owner can mint tokens 
-    it("Should set the right unlockTime", async function () {
-      const { hardhatExpense, owner, addr1 } = await loadFixture(deployContract);
+    it("owner mint tokens", async function () {
+      const { hardhatExpense, owner, addr1, mockUSDC} = await loadFixture(deployContract);
 
-      const ownerBalance = await hardhatExpense.mint(owner.address, 100000);
+      await hardhatExpense.mint(owner.address, 100000); // Removed assignment to ownerBalance variable
+      const ownerBalance = await hardhatExpense.balanceOf(owner.address); // Use balanceOf function to get the balance
       expect(await hardhatExpense.totalSupply()).to.equal(ownerBalance);
     });
     // owner mint and give them to a differnet user
-    it("mint tokens to other person", async function () {
-      const { hardhatExpense, owner, addr1 } = await loadFixture(deployContract);
+    it("mint tokens to addr1", async function () {
+      const { hardhatExpense, owner, addr1, mockUSDC} = await loadFixture(deployContract);
 
-      const addr1Balance = await hardhatExpense.mint(addr1.address, 100000);
-      expect(await hardhatExpense.totalSupply()).to.equal(addr1Balance);
+      await hardhatExpense.mint(addr1.address, ethers.utils.parseUnits("100000", 18));
+      const addr1Balance = await hardhatExpense.balanceOf(addr1.address); // Use balanceOf function to get the balance
+      expect(addr1Balance).to.equal(ethers.utils.parseUnits("100000", 18));
     });
 
     //owner can put USDC into the contract
-    it("owner can put USDC into the contract", async function () {
-      const { hardhatExpense, owner, addr1 } = await loadFixture(deployContract);
+    it("User can put USDC inside the contract and owner can swap EXP for USDC", async function () {
+      const { hardhatExpense, owner, addr1, mockUSDC} = await loadFixture(deployContract);
+    // Transfer USDC tokens from addr1 to Expense contract
+      await mockUSDC.connect(addr1).approve(hardhatExpense.address, ethers.utils.parseUnits("50000", 6));
+      await hardhatExpense.connect(addr1).fundUSDC(ethers.utils.parseUnits("50000", 6));
 
-      const ownerBalance = await hardhatExpense.mint(owner.address, 100000);
-      expect(await hardhatExpense.totalSupply()).to.equal(ownerBalance);
-    });
+      // Verify USDC balance inside the Expense contract
+      console.log(await mockUSDC.balanceOf(hardhatExpense.address));
+      console.log(ethers.utils.parseUnits("50000", 6));
+      expect(await mockUSDC.balanceOf(hardhatExpense.address)).to.equal(ethers.utils.parseUnits("50000", 6));
 
-    
+      console.log(await hardhatExpense.balanceOf(owner.address));
+      // Swap EXP tokens for USDC tokens using owner
+      await hardhatExpense.connect(owner).approve(hardhatExpense.address, ethers.utils.parseUnits("50000", 6));
+      await hardhatExpense.connect(owner).exchangeEXP(ethers.utils.parseUnits("50000", 6));
+
+      // Verify the balance change of USDC and EXP tokens for owner
+      expect(await mockUSDC.balanceOf(owner.address)).to.equal(ethers.utils.parseUnits("50000", 6));
+      // expect(await hardhatExpense.balanceOf(owner.address)).to.equal(ethers.utils.parseUnits("99000"));
   });
 
-  // describe ("Deployment", function () {
-
-  // it("Deployment + mint tokens to owner, check its equal", function () {
-  //   const hardhatExpense = await Token.deploy();
-
-  //   const ownerBalance = await hardhatToken.mint(owner.address, 100000);
-  //   expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
-  // });
-
-  // it("test send tokens to secondary address", async function () {
-  //   const [owner] = await ethers.getSigners();
-
-  //   const Token = await ethers.getContractFactory("Token");
-
-  //   const hardhatToken = await Token.deploy();
-
-  //   const ownerBalance = await hardhatToken.mint(owner.address, 100000);
-  //   expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
-  // });
-
-  // });
-
-  //test that owner can mint tokens and give them to a differnet user
-
-
-
-  //owner can put USDC into the contract
-
-  //different user can exchange EXP for USDC
+  });
 
 
 });
