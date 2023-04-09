63b4ce65df05f865eefa1f7b4d1e9008bc9347ca
Derrick Cui <derrick_cui@berkeley.edu>
2023-04-09 14:19:23 -0700

    done contract!

--- MatchaSmartContract/hardhat.config.js
+++ MatchaSmartContract/hardhat.config.js
@@ -1,11 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+// Go to https://alchemy.com, sign up, create a new App in
+// its dashboard, and replace "KEY" with its key
+const ALCHEMY_API_KEY = "UmYyhfdOe6gl6axZ01mts0Egh9FC0utA";
+
+// Replace this private key with your Sepolia account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Beware: NEVER put real Ether into testing accounts
+const SEPOLIA_PRIVATE_KEY = "7036d977062a8a4534c8cdfda9005bfe47ba1c7cbab22290c3bf610b5bbbd5c2";
+
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.9",
   networks: {
-    ganache: {
-      url: "HTTP://127.0.0.1:7545",
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    }
   }
-}
 };
