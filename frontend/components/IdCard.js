b081a26cbfa0a0c1bf7d9b4f5cd9687060900860
Trevor Trinh <50549133+trevor-trinh@users.noreply.github.com>
2023-04-26 19:01:23 -0700

    ty brian <3

--- frontend/components/IdCard.js
+++ frontend/components/IdCard.js
@@ -2,9 +2,9 @@ const IdCard = () => {
   return (
     <div class="relative">
       <img
-        src={'images/DaddyTimCard.png'}
+        src={'images/BrianCard.png'}
         alt="My Image"
-        className="w-[24rem] rounded-md "
+        className="w-[24rem] rounded-md"
       />
       <img
         src={'images/Bitcoinfloaty.png'}
@@ -16,12 +16,3 @@ const IdCard = () => {
 };
 
 export default IdCard;
-{
-  /* <img
-src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
-alt="App screenshot"
-width={2432}
-height={1442}
-className="w-[76rem] rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10"
-/> */
-}
