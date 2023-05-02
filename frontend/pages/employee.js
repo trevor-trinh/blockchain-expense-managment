b1341678b8dee75635396792fcac026e3c57a244
Derrick Cui <derrick_cui@berkeley.edu>
2023-05-02 02:00:27 -0700

    done the PLAID integration fuck plaid so shit

--- frontend/pages/employee.js
+++ frontend/pages/employee.js
@@ -1,14 +1,51 @@
 import Layout from '@/components/Layout';
+import Router from 'next/router';
+import { useState, useEffect, useCallback } from 'react';
+import { usePlaidLink } from 'react-plaid-link';
 
-export default function Signup() {
-  return (
+export default function PlaidLink() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const createLinkToken = async () => {
+      const response = await fetch('/api/create-link-token', {
+        method: 'POST'
+      });
+      const { link_token } = await response.json();
+      setToken(link_token);
+    };
+    createLinkToken();
+  }, []);
+
+  const onSuccess = useCallback(async publicToken => {
+    await fetch('/api/exchange-public-token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ public_token: publicToken })
+    });
+    Router.push('/dash');
+  }, []);
+
+  const { open } = usePlaidLink({
+    token,
+    onSuccess
+  });
+
+   return (
     <Layout>
       <div class="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <h1 className="text-3xl font-bold tracking-tight text-white text-center">
           Employee Page
         </h1>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={open}>
+          Connect to Plaid
+        </button>
         <div className="mt-10"></div>
       </div>
     </Layout>
-  );
+   );
 }
